350ec6a1b5e7688ac4eaf18534f36e4f98c7eb40
Tiago Verçosa <tiago.vercosa@gmail.com>
2021-06-09 23:31:01 -0300

    local bin files

---
+++ .local/bin/dmenu-logout.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+first_prompt="O que você quer?"
+opt1="1. Reiniciar\n2. Desligar"
+opt2="Sim\nNão"
+cor1="#0077bb"
+azul="#0088CC"
+red="#8A2A0B"
+
+ask() {
+    answer="$( echo -e $1 | dmenu -i -p "$2" -fn 'FuraCode Nerd Font-11 #999' -nb '#292d3e' -sf '#eee' -sb "$azul" -l 2 )"
+}
+
+ask "$opt1" "$first_prompt"
+
+case $answer in
+    "1. Reiniciar")
+        ask "$opt2" "Quer mesmo reiniciar o sistema?"
+        if [[ $answer == "Sim" ]]; then
+            systemctl reboot
+        fi
+        exit 0
+        ;;
+    "2. Desligar")
+        ask "$opt2" "Quer mesmo desligar o computador?"
+        if [[ $answer == "Sim" ]]; then
+            systemctl poweroff
+        fi
+        exit 0
+        ;;
+esac
+
+exit 0
---
+++ .local/bin/dmenu-monitor.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+ask="Qual monitor?"
+opt="1. Interno\n2. Externo\n3. Lado a lado"
+red="#8A2A0B"
+azul="#0088CC"
+
+ask() {
+    answer="$( echo -e $1 | dmenu -fn 'FuraCode Nerd Font-11 #999' -nb '#292d3e' -sf '#eee' -sb "$azul" -p "$2" -l 3)"
+}
+
+ask "$opt" "$ask"
+
+case $answer in
+    "1. Interno")
+        if [[ $answer == "1. Interno" ]]; then
+            ${HOME}/.screenlayout/mobile.sh && ${HOME}/.fehbg
+        fi
+        exit 0
+        ;;
+    "2. Externo")
+        if [[ $answer == "2. Externo" ]]; then
+            ${HOME}/.screenlayout/docked-home.sh && ${HOME}/.fehbg
+        fi
+        exit 0
+        ;;
+    "3. Lado a lado")
+        if [[ $answer == "3. Lado a lado" ]]; then
+            ${HOME}/.screenlayout/side-by-side.sh && ${HOME}/.fehbg
+        fi
+        exit 0
+        ;;
+esac
+
+exit 0
