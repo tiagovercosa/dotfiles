a63f5f02d409563d4f0b6bf32678d19b380737aa
Tiago Verçosa <vercosa@protonmail.com>
2022-02-01 12:13:24 -0300

    reduce script

--- .local/bin/monitor.sh
+++ .local/bin/monitor.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-inter=eDP1
-exter=HDMI1
-
-if xrandr | grep "$exter disconnected"; then
-  xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --off && nitrogen --restore
-else
-  xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off && nitrogen --restore
+if xrandr --listmonitors | grep HDMI1; then
+  xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 fi
