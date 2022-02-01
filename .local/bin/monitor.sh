b668d203e029414561f8543ed245c86474de856b
Tiago Verçosa <vercosa@protonmail.com>
2022-02-01 19:31:14 -0300

    added sway

--- .local/bin/monitor.sh
+++ .local/bin/monitor.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-if xrandr --listmonitors | grep HDMI1; then
-  xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+# if xrandr --listmonitors | grep HDMI1; then
+#   xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+# fi
+
+if swaymsg -t get_outputs | grep HDMI-A-1; then
+  swaymsg output eDP-1 disable
 fi
