94f93a622b07f2c715e1e6867fa1a2914fcac734
Tiago Verçosa <vercosa@protonmail.com>
2022-02-12 11:42:07 -0300

    enw config for monitor switch

--- .local/bin/screenlayout/docked-home.sh
+++ .local/bin/screenlayout/docked-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off
