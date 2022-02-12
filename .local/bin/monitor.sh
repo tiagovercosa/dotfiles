f4b9aa57d1e1c9425979fc7f195e8a7b9aeaee32
Tiago Verçosa <vercosa@protonmail.com>
2022-02-12 11:44:48 -0300

    new config for monitor

--- .local/bin/monitor.sh
+++ .local/bin/monitor.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# if xrandr --listmonitors | grep HDMI1; then
-#   xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-# fi
-
-if swaymsg -t get_outputs | grep HDMI-A-1; then
-  swaymsg output eDP-1 disable
+if xrandr --listmonitors | grep HDMI1; then
+  xrandr --output eDP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off
 fi
