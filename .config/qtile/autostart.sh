6331792a1a777b2c37e4dfae95b0ffe84392f2d7
Tiago Verçosa <tiago.vercosa@outlook.com>
2022-04-12 20:51:54 +0000

    update lat:lat at redshift

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 picom -b --config ~/.config/picom/picom.conf &
 xset r rate 300 50 &
-redshift-gtk &
-# redshift -l -31.8:-52.3 -t 5700:3600 -g 0.8 -m randr &
-setxkbmap br abnt2 &
+redshift -l -8.01:-34.90 -t 5700:3600 -g 0.8 -m randr &
+setxkbmap br &
 nm-applet --indicator &
-pcmanfm -d &
 dunst &
-xss-lock -l -- i3lock -c 000000 &
+xss-lock -l -- $HOME/.local/bin/myi3lock &
