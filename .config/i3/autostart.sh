80d12ebe181684b00d20901e01e6d5017adbe049
Tiago Verçosa <vercosa@proton.me>
2022-07-24 10:40:39 -0300

    added autostart file

---
+++ .config/i3/autostart.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+nitrogen --restore &
+picom -b --config ~/.config/picom/picom.conf --vsync &
+xset r rate 300 50 &
+redshift -l -7.15:-34.84 -t 5700:4000 -g 0.8 -m randr &
+/usr/lib/polkit-kde-authentication-agent-1 &
