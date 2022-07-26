3285ddef2aac1f08792c590b16abd78864246e5a
Tiago Verçosa <vercosa@proton.me>
2022-07-26 18:53:10 -0300

    added dunst autostart and config print flameshot

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -5,3 +5,4 @@ picom -b --config ~/.config/picom/picom.conf --vsync &
 xset r rate 300 50 &
 redshift -l -7.15:-34.84 -t 5700:4000 -g 0.8 -m randr &
 /usr/lib/polkit-kde-authentication-agent-1 &
+dunst -conf ~/.config/dunst/dunstrc &
