b6d304d5bef26c02b92f59848de5bd376b1cdadb
Tiago Verçosa <vercosa@proton.me>
2022-08-18 11:11:07 -0300

    changed named files

--- .config/zsh/.zprofile
+++
@@ -1,3 +0,0 @@
-# if systemctl -q is-active graphical.target && [[ ! $DISPLAY && "$(tty)" = "/dev/tty1" ]]; then
-#   exec startx "$XINITRC"
-# fi
--- .local/bin/monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if xrandr --listmonitors | grep HDMI1; then
-  xrandr --output eDP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off
-fi
