9b37c8e7861bfe741ce0b881216980d54ad9c66f
Tiago Lima <vercosa@proton.me>
2026-05-03 16:46:30 -0300

    Update autopairs.lua

--- macos/nvim/.config/nvim/lua/plugins/autopairs.lua
+++ macos/nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -3,6 +3,9 @@ return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = function()
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+
       require('nvim-autopairs').setup({
         disable_filetype = { "TelescopePrompt", "vim" },
         disable_in_macro = true,
@@ -11,6 +14,32 @@ return {
           map = '<M-e>',
         },
       })
+
+      npairs.add_rules {
+        Rule(' ', ' ')
+        :with_pair(function (opts)
+          local pair = opts.line:sub(opts.col - 1, opts.col)
+          return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+        end),
+        Rule('( ', ' )')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+        end)
+        :use_key(')'),
+        Rule('{ ', ' }')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+        end)
+        :use_key('}'),
+        Rule('[ ', ' ]')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+        end)
+        :use_key(']')
+      }
     end
   }
 }
