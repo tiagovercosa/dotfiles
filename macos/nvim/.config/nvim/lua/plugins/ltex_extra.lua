1af7a7197d06b3d9be26dfb10a661095571861ea
Tiago Lima <vercosa@proton.me>
2026-05-03 16:09:24 -0300

    Update ltex_extra.lua

--- macos/nvim/.config/nvim/lua/plugins/ltex_extra.lua
+++ macos/nvim/.config/nvim/lua/plugins/ltex_extra.lua
@@ -2,27 +2,34 @@ return {
   "barreiroleo/ltex_extra.nvim",
   ft = { "markdown", "tex" },
   config = function()
-    require("ltex_extra").setup({
-      load_langs = { "pt-BR", "en-US" },
-      init_check = false,
-      path = vim.fn.stdpath("data") .. "/ltex",
-      server_start = false,
-    })
-
     vim.lsp.config("ltex_plus", {
       cmd = { "ltex-ls-plus" },
       settings = {
         ltex = {
-          language = { "pt-BR", "en-US" },
+          language = "pt-BR",
           languageToolHttpServerUri = "https://api.languagetoolplus.com/",
-          languageToolOrganizationId = "OrganizationId",
-          languageToolApiKey = "API-KEY",
+          languageToolOrg = {
+            username = "USER-NAME",
+            apiKey = "API-KEY",
+          },
         },
       },
-      on_attach = function(client, bufnr)
-        require("ltex_extra").reload({ "pt-BR", "en-US" })
+    })
+
+    vim.lsp.enable("ltex_plus")
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        if client and client.name == "ltex_plus" then
+          require("ltex_extra").setup({
+            load_langs = { "pt-BR", "en-US" },
+            init_check = true,
+            path = vim.fn.stdpath("data") .. "/ltex",
+          })
+        end
       end,
     })
-    vim.lsp.enable("ltex_plus")
   end,
 }
