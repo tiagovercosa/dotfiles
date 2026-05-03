fe2afdea76eeab8c50606a05054bedbbfc307c4a
Tiago Lima <vercosa@proton.me>
2026-05-03 16:07:24 -0300

    update for lulatex

--- macos/nvim/.config/nvim/lua/plugins/latex.lua
+++ macos/nvim/.config/nvim/lua/plugins/latex.lua
@@ -2,26 +2,18 @@ return {
   {
     "lervag/vimtex",
     lazy = false,
+    ft = "tex",
     config = function()
       vim.g.vimtex_view_method = "skim"
       vim.g.vimtex_view_skim_sync = 1
-      vim.g.vimtex_view_skim_activate = 1
-
-      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_view_skim_activate = 0
       vim.g.vimtex_quickfix_mode = 0
-
+      vim.g.tex_flavor = "latex"
       vim.g.tex_conceal = "abdmg"
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "tex",
-        callback = function()
-          vim.opt_local.conceallevel = 1
-        end,
-      })
 
-      vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_compiler_latexmk = {
         options = {
-          "-pdf",
+          "-lualatex",
           "-shell-escape",
           "-verbose",
           "-file-line-error",
