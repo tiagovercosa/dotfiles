c12309e02786b82bd0322edecd0415e00f74784e
Tiago Lima <vercosa@proton.me>
2026-05-10 21:22:59 -0300

    background false

--- macos/nvim/.config/nvim/lua/plugins/nord.lua
+++ macos/nvim/.config/nvim/lua/plugins/nord.lua
@@ -7,7 +7,7 @@ return {
 
       vim.g.nord_contrast = true
       vim.g.nord_borders = false
-      vim.g.nord_disable_background = true
+      vim.g.nord_disable_background = false
       vim.g.nord_italic = true
       vim.g.nord_uniform_diff_background = true
       vim.g.nord_bold = true
