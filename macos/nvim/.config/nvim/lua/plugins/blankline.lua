b1332fb365687966116ffe57fcf4db6ee3dbd422
Tiago Lima <vercosa@proton.me>
2026-05-10 21:21:59 -0300

    Create blankline.lua

---
+++ macos/nvim/.config/nvim/lua/plugins/blankline.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function ()
+      vim.api.nvim_set_hl(0, "Whitespace", { fg = "#4C566A" })
+      vim.api.nvim_set_hl(0, "IblIndent", { fg = "#4C566A" })
+      vim.api.nvim_set_hl(0, "IblScope", { fg = "#88C0D0" })
+
+      require("ibl").setup({
+        indent = {
+          char = "│",
+        },
+        scope = {
+          enabled = true,
+          char = "│",
+          show_start = false,
+          show_end = false,
+      },
+      whitespace = {
+        highlight = { "Whitespace" },
+        remove_blankline_trail = false,
+      }
+    })
+    end
+  }
+}
