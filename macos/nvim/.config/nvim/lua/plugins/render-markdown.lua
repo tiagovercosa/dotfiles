a49c8b24751baa42a784ceaed1d357852ec308ed
Tiago Lima <vercosa@proton.me>
2026-05-13 23:46:13 -0300

    Create render-markdown.lua

---
+++ macos/nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "rmd", "obsidian", "quarto" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+
+    opts = {
+      heading = {
+        enabled = true,
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        border = true,
+      },
+    },
+
+    code = {
+      enabled = true,
+      sign = true,
+      style = "full",
+      width = "block",
+      right_pad = 1,
+    },
+
+    bullet = {
+      enabled = true,
+      icons = { "●", "○", "◆", "◇" },
+    },
+
+    checkbox = {
+      enabled = true,
+      unchecked = { icon = "󰄱 " },
+      checked = { icon = "󰱒 " },
+      -- Estilo opcional para tarefas em andamento (ex: [-])
+      custom = {
+        todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownInfo" },
+      },
+    },
+
+    pipe_table = {
+      enabled = true,
+      preset = "heavy", -- Deixa as bordas da tabela mais marcadas
+    },
+  }
+}
