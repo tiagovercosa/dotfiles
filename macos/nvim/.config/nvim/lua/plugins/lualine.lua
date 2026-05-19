1b3d37188652dbca70c31589db969b052f05eca9
Tiago Lima <vercosa@proton.me>
2026-05-18 23:45:46 -0300

    theme nord

--- macos/nvim/.config/nvim/lua/plugins/lualine.lua
+++ macos/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -47,7 +47,7 @@ return {
       require("lualine").setup({
         options = {
           icons_enabled = true,
-          theme = "onenord",
+          theme = "nord",
           component_separators = { left = '|', right = '|' },
           section_separators = { left = '', right = '' },
           always_divide_middle = true,
