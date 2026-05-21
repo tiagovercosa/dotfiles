5286593a87eeea7900b08900b04ac39dc406745a
Tiago Lima <vercosa@proton.me>
2026-05-21 14:49:08 -0300

    Update vimtex plugin configuration for Neovim

--- macos/nvim/.config/nvim/lua/plugins/latex.lua
+++ macos/nvim/.config/nvim/lua/plugins/latex.lua
@@ -1,28 +1,30 @@
 return {
-  {
-    "lervag/vimtex",
-    lazy = true,
-    ft = "tex",
-    config = function()
-      vim.g.vimtex_view_method = "skim"
-      vim.g.vimtex_view_skim_sync = 1
-      vim.g.vimtex_view_skim_activate = 0
-      vim.g.vimtex_quickfix_mode = 0
-      vim.g.vimtex_fold_enabled = 1
-      vim.g.tex_flavor = "latex"
-      vim.g.tex_conceal = "abdmg"
+  "lervag/vimtex",
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 0
+    vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_fold_enabled = 1
+    vim.g.tex_flavor = "latex"
+    vim.g.tex_conceal = "abdmg"
 
-      vim.g.vimtex_compiler_latexmk = {
-        options = {
-          "-lualatex",
-          "-shell-escape",
-          "-verbose",
-          "-file-line-error",
-          "-synctex=1",
-          "-interaction=nonstopmode",
-        },
-      }
-    end,
-  },
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = "build",
+      options = {
+        "-lualatex",
+        "-shell-escape",
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+      },
+      callback = 1,
+      continuous = 1,
+      executable = "latexmk",
+      hooks = {},
+    }
+  end,
 }
 
