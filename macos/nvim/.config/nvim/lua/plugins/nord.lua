a33cf25bf5dd12674114d2c4af8206f7b8c60625
Tiago Lima <vercosa@proton.me>
2026-05-20 21:51:47 -0300

    fix cursor line transparent

--- macos/nvim/.config/nvim/lua/plugins/nord.lua
+++ macos/nvim/.config/nvim/lua/plugins/nord.lua
@@ -6,7 +6,7 @@ return {
     vim.g.nord_contrast = true                   -- Make sidebars and popup menus like nvim-tree and telescope have a different background
     vim.g.nord_borders = true                   -- Enable the border between verticaly split windows visable
     vim.g.nord_disable_background = true         -- Disable the setting of background color so that NeoVim can use your terminal background
-    vim.g.set_cursorline_transparent = false     -- Set the cursorline transparent/visible
+    vim.g.nord_cursorline_transparent = false     -- Set the cursorline transparent/visible
     vim.g.nord_italic = true                    -- enables/disables italics
     vim.g.nord_enable_sidebar_background = true -- Re-enables the background of the sidebar if you disabled the background of everything
     vim.g.nord_uniform_diff_background = true    -- enables/disables colorful backgrounds when used in diff mode
