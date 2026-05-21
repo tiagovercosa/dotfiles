587f1394bebc98b0078f8e27348477ea30393bff
Tiago Lima <vercosa@proton.me>
2026-05-20 21:52:36 -0300

    add core.ui2

--- macos/nvim/.config/nvim/init.lua
+++ macos/nvim/.config/nvim/init.lua
@@ -1,12 +1,12 @@
-require 'core.options'
-require 'core.keymaps'
+require('vim._core.ui2').enable({
+  enable = true,
+  msg = {
+    target = "cmd",
+    pager = { height = 1 },
+    msg = { height = 0.5, timeout = 5000 },
+    dialog = { height = 0.5},
+    cmd = { height = 0.5 },
+  },
+})
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
+require("config.lazy")
