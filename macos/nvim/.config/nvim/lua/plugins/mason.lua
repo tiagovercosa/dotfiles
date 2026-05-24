6c761ca628f70e6df3fd3068ea7add8431de31b6
Tiago Lima <vercosa@proton.me>
2026-05-24 18:21:21 -0300

    Lazy-load mason.nvim via Mason commands
    Add a cmd field to the williamboman/mason.nvim plugin spec so the plugin is only loaded when Mason commands are invoked (Mason, MasonInstall, MasonUninstall, MasonUpdate, MasonLog). This defers initialization until needed and can improve startup performance.

--- macos/nvim/.config/nvim/lua/plugins/mason.lua
+++ macos/nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
   {
     "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUpdate", "MasonLog" },
     config = function()
       require("mason").setup()
     end,
