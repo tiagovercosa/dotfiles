d63b65eef80a0732f65ff2f6d7a0d40ffc4c28d5
Tiago Lima <vercosa@proton.me>
2026-05-24 17:53:06 -0300

    Update autocmd.lua

--- macos/nvim/.config/nvim/lua/config/autocmd.lua
+++ macos/nvim/.config/nvim/lua/config/autocmd.lua
@@ -3,6 +3,8 @@ local last_cursor_group = vim.api.nvim_create_augroup("LastCursorGroup", {})
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = last_cursor_group,
   callback = function()
+    local ft = vim.bo.filetype
+    if ft == "gitcommit" or ft == "gitrebase" then return end
     local last_pos = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
     if last_pos[1] > 0 and last_pos[1] <= lcount then
@@ -18,14 +20,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function()
     vim.hl.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
-})
-
--- LaTeX
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "tex",
-  callback = function()
-    vim.opt_local.conceallevel = 2
   end,
 })
 
