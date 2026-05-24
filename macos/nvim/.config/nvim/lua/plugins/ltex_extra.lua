5adfd381fd2a4a1f8f69b749ec9c7632f9fe671c
Tiago Lima <vercosa@proton.me>
2026-05-24 17:53:58 -0300

    Update ltex_extra.lua

--- macos/nvim/.config/nvim/lua/plugins/ltex_extra.lua
+++ macos/nvim/.config/nvim/lua/plugins/ltex_extra.lua
@@ -2,20 +2,6 @@ return {
   "barreiroleo/ltex_extra.nvim",
   ft = { "markdown", "tex" },
   config = function()
-    vim.lsp.config("ltex_plus", {
-      cmd = { "ltex-ls-plus" },
-      settings = {
-        ltex = {
-          language = "pt-BR",
-          languageToolHttpServerUri = "https://api.languagetoolplus.com/",
-          languageToolOrg = {
-            username = vim.env.LTEX_LT_USERNAME,
-            apiKey = vim.env.LTEX_LT_APIKEY,
-          },
-        },
-      },
-    })
-
     vim.lsp.enable("ltex_plus")
 
     vim.api.nvim_create_autocmd("LspAttach", {
