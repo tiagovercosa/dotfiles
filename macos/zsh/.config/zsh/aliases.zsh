b792c98a491cffbe952fe9ad1ea51638b1d353ec
Tiago Lima <vercosa@proton.me>
2026-06-01 00:47:43 -0300

    Remove editor and SSH aliases from zsh config

--- macos/zsh/.config/zsh/aliases.zsh
+++ macos/zsh/.config/zsh/aliases.zsh
@@ -24,14 +24,6 @@ alias grep="rg --colors 'match:fg:magenta' --colors 'match:style:bold'"
 alias fgrep='rg -F'
 alias diff='diff --color=auto'
 
-# Editor
-alias vi='nvim'
-
-# Cluster LCCMat
-alias cluster='TERM=xterm-256color ssh -p80 tiago@164.41.10.150'
-alias lccmat='TERM=xterm-256color ssh -X tiago@100.73.56.10'
-alias imac='TERM=xterm-256color ssh -X tiagolima@100.75.126.24'
-
 # Apps
 alias qtgrace='/Applications/qtgrace.app/Contents/MacOS/qtgrace'
 alias vmd='/Applications/VMD2b1.app/Contents/MacOS/startup.command'
